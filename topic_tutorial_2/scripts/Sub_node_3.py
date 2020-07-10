5f8232be0cd2400ef09f538dd6cd87de010cbbe2
bvcbv <bvcbv@naver.com>
2020-07-10 11:16:05 +0900

    jin wecar

---
+++ topic_tutorial_2/scripts/Sub_node_3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import rospy
+from topic_tutorial_2.msg import MyMsgs
+
+NAME_TOPIC = '/msgs_talk'
+NAME_NODE = 'Sub_node_3'
+
+def callback(msgs):
+    rospy.loginfo(-msgs.x + 3*msgs.y)
+
+if __name__ == '__main__':
+    rospy.init_node(NAME_NODE, anonymous=True)
+    sub = rospy.Subscriber(NAME_TOPIC, MyMsgs, callback)
+    rospy.spin()
